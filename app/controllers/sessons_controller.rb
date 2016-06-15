cc73c51af070c3155f50e8fa551c406827cb821d
dbicanic <dbicanic77@gmail.com>
2016-06-15 18:01:31 -0500

    Even more fixes

--- app/controllers/sessons_controller.rb
+++ app/controllers/sessons_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
   def create
 
-    user = User.find_by(email: session_params[:email])
+    teacher = Teacher.find_by(email: session_params[:email])
 
-    if user && user.authenticate(session_params[:password])
+    if teacher && teacher.authenticate(session_params[:password])
       session[:teacher_id] = teacher.id
       redirect_to root_path
     else
