3e3b195605ba277c1879eaa471820b7017dba853
dbicanic <dbicanic77@gmail.com>
2016-06-15 16:15:57 -0500

    Add in correct routes to sessions

---
+++ app/controllers/sessons_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def create
+
+    user = User.find_by(email: session_params[:email])
+
+    if user && user.authenticate(session_params[:password])
+      session[:teacher_id] = teacher.id
+      redirect_to root_path
+    else
+      @message = "Sorry, we had some errors..."
+      render :new
+    end
+
+  end
+
+  def destroy
+    session.clear
+    redirect_to "sessions#new"
+  end
+
+  private
+    def session_params
+      params.permit(:email, :password)
+    end
+end
