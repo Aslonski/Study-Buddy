e48cc326fbb8c7c0a506de47ab40d6d7b19f18c9
Andrey Slonski & David Bicanic & dbravman & Jill Moore & SangMee Specht <pair+aslonski.dbicanic.dbravman.okjill.sangmeespecht@devbootcamp.com>
2016-06-15 15:57:43 -0500

    Add models, migrations and associations for teachers and students

---
+++ db/migrate/20160615205147_create_teachers.rb
@@ -0,0 +1,12 @@
+class CreateTeachers < ActiveRecord::Migration
+  def change
+    create_table :teachers do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.boolean :admin
+
+      t.timestamps null: false
+    end
+  end
+end
