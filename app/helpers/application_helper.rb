9db3336de582038f69e422a14de41592295fa372
dbicanic <dbicanic77@gmail.com>
2016-06-15 16:17:01 -0500

    Add in helper methods curret user, logged in? and check logged in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def current_user
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def logged_in?
+    session[:user_id] != nil
+  end
+
+  def check_logged_in
+    redirect_to new_session_path unless logged_in?
+  end
 end
