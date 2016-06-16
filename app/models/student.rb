d7027d05b22d1cf7bf0af7d7c89389e3ef0f0ffd
Andrey Slonski & David Bicanic & dbravman & Jill Moore & SangMee Specht <pair+aslonski.dbicanic.dbravman.okjill.sangmeespecht@devbootcamp.com>
2016-06-15 19:22:21 -0500

    Add 19 model tests

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,6 +6,8 @@ class Student < ActiveRecord::Base
 
    validates :grade, numericality: {only_integer: true}
 
+   validates :age, numericality: {only_integer: true}
+
    validates :gpa, inclusion: { in: 0..4 }
 
 
@@ -15,5 +17,4 @@ class Student < ActiveRecord::Base
    validates :gpa, numericality: { :less_than_or_equal_to => 4.0 }
    validates :gpa, numericality: { :greater_than_or_equal_to => 0 }
 
-   validates :age, numericality: {only_integer: true}
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe Student, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+
+describe Student do
+  it { should belong_to(:teacher) }
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:age) }
+  it { should validate_presence_of(:gpa) }
+  it { should validate_presence_of(:grade) }
+  it { should validate_presence_of(:disciplinary_strikes) }
+  it { should validate_presence_of(:shirt_size) }
+
+  it { should validate_numericality_of(:age).only_integer }
+  it { should validate_numericality_of(:grade).only_integer }
+  it { should validate_numericality_of(:grade).is_less_than_or_equal_to(12) }
+  it { should validate_numericality_of(:grade).is_greater_than_or_equal_to(0) }
+  it { should validate_numericality_of(:gpa).is_less_than_or_equal_to(4.0) }
+  it { should validate_numericality_of(:gpa).is_greater_than_or_equal_to(0) }
+  it { should validate_inclusion_of(:gpa).in_range(0..4) }
+
 end
