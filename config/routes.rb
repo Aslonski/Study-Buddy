9aa14a4378e17c3bdc14886510ae94fab7758d49
Andrey Slonski & dbravman <pair+Aslonski.dbravman@devbootcamp.com>
2016-06-16 10:05:34 -0500

    Add 1 feature test for login and before action check

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :students
   resources :teachers, except: [:update]
-  root "students#index"
+  root "sessions#new"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
