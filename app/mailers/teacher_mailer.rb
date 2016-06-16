e6b235c218c0dea27f57cbd772a59e93bf7d1090
Andrey Slonski & David Bicanic & dbravman & Jill Moore & SangMee Specht <pair+Aslonski.dbicanic.dbravman.okjill.sangmeespecht@devbootcamp.com>
2016-06-16 18:04:24 -0500

    Add TeacherMailer debugger

--- app/mailers/teacher_mailer.rb
+++ app/mailers/teacher_mailer.rb
@@ -1,5 +1,7 @@
 class TeacherMailer < ApplicationMailer
   def self.account_activation(teacher)
+    p "========="
+    p teacher
     @teacher = teacher
     mail to: @teacher.email, subject: "Welcome to Study Buddy!"
   end
