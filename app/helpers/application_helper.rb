857c81e12e8081ae458387ac82636d3d38008930
Andrey Slonski & dbravman <pair+Aslonski.dbravman@devbootcamp.com>
2016-06-16 11:34:22 -0500

    deleting students tests pass

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,6 @@ module ApplicationHelper
   end
 
   def check_logged_in
-    redirect_to new_session_path unless logged_in?
+    redirect_to root_path unless logged_in?
   end
 end
--- spec/features/deleteing_students_spec.rb
+++ spec/features/deleteing_students_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 feature "Deleting students" do
+  scenario "Admin deletes 1 student" do
+    Teacher.create!(name: "Danny", email: "me@me.me", password: "password", admin: true)
 
+    student_to_delete = Student.create!(first_name: "Student", last_name: "StudentA", disciplinary_strikes: 0, grade: 4, age: 10, gpa: 3, shirt_size: "S")
+    Student.create!(first_name: "Student", last_name: "StudentB", disciplinary_strikes: 0, grade: 4, age: 10, gpa: 3, shirt_size: "S")
+    Student.create!(first_name: "Student", last_name: "StudentC", disciplinary_strikes: 0, grade: 4, age: 10, gpa: 3, shirt_size: "S")
+
+    visit '/'
+
+    fill_in "Email", :with => "me@me.me"
+    fill_in "Password", :with => "password"
+    click_button "Login"
+
+    visit "/students"
+
+    find(:css, "input[type='checkbox'][value='#{student_to_delete.id}']").set(true)
+    click_button "Delete Student(s)"
+
+    visit "/students"
+
+    expect(page).to_not have_text("StudentA")
+    expect(page).to have_text("StudentB")
+    expect(page).to have_text("StudentC")
+
+  end
+
+  scenario "Admin deletes multiple students" do
+    Teacher.create!(name: "Danny", email: "me@me.me", password: "password", admin: true)
+
+    student1 = Student.create!(first_name: "Student", last_name: "StudentA", disciplinary_strikes: 0, grade: 4, age: 10, gpa: 3, shirt_size: "S")
+    student2 = Student.create!(first_name: "Student", last_name: "StudentB", disciplinary_strikes: 0, grade: 4, age: 10, gpa: 3, shirt_size: "S")
+    Student.create!(first_name: "Student", last_name: "StudentC", disciplinary_strikes: 0, grade: 4, age: 10, gpa: 3, shirt_size: "S")
+
+    visit '/'
+
+    fill_in "Email", :with => "me@me.me"
+    fill_in "Password", :with => "password"
+    click_button "Login"
+
+    visit "/students"
+
+    find(:css, "input[type='checkbox'][value='#{student1.id}']").set(true)
+    find(:css, "input[type='checkbox'][value='#{student2.id}']").set(true)
+    click_button "Delete Student(s)"
+
+    visit "/students"
+
+    expect(page).to_not have_text("StudentA")
+    expect(page).to_not have_text("StudentB")
+    expect(page).to have_text("StudentC")
+  end
 end
