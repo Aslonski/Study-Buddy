2aed55496c075fc8e2e7ec713a5ca7df09232ea8
SangMeeSpecht <[01;31m[K"email"[m[K: null>
2016-07-12 17:00:16 -0500

    update environment host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: "still-retreat-46802.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: "dbcstudybuddy.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
